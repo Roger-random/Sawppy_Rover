bf283326b3e50c7107a68aa661d7e55a0d71818f
Roger Cheng <roger.random@outlook.com>
2021-03-07 16:14:22 -0800

    Stop using awkward 'half' measurements

--- esp32_sawppy/lib/rover/src/wheel_parameter.h
+++ esp32_sawppy/lib/rover/src/wheel_parameter.h
@@ -23,20 +23,20 @@ static const float wheelbase_front  = 0.14;
 // Distance from back wheel to mid wheel.
 static const float wheelbase_back   = 0.14;
 
-// Half of distance between left and right front wheels
-static const float track_half_front = 0.1;
+// Distance between left and right front wheels
+static const float track_front = 0.2;
 
-// Half of distance between left and right mid wheels
-static const float track_half_mid   = 0.135;
+// Distance between left and right mid wheels
+static const float track_mid   = 0.27;
 
-// Half of distance between left and right back wheels
-static const float track_half_back  = 0.1;
+// Distance between left and right back wheels
+static const float track_back  = 0.2;
 
 // X and Y coordinate of a wheel relative to rover center
 typedef struct wheel_position
 {
   float x; // One of wheelbase_* values
-  float y; // One of track_half_* values
+  float y; // One of track_* values, divided by two
 } wheel_position;
 
 // Array of wheel positions relative to rover center
@@ -44,32 +44,32 @@ const wheel_position wheel_positions[wheel_count] = {
   // front_left
   {
     wheelbase_front,
-    track_half_front,
+    track_front/2,
   },
   // front_right
   {
     wheelbase_front,
-    -track_half_front,
+    -track_front/2,
   },
   // mid_left
   {
     0,
-    track_half_mid,
+    track_mid/2,
   },
   // mid_right
   {
     0,
-    -track_half_mid,
+    -track_mid/2,
   },
   // back_left
   {
     -wheelbase_back,
-    track_half_back,
+    track_back/2,
   },
   // back_right
   {
     -wheelbase_back,
-    -track_half_back,
+    -track_back/2,
   },
 };
 
