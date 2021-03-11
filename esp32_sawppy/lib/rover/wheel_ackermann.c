53d7babbb901983106fd0401fecd918ca359a413
Roger Cheng <roger.random@outlook.com>
2021-03-10 20:59:27 -0800

    Wheel Ackermann

--- esp32_sawppy/lib/rover/wheel_ackermann.c
+++ esp32_sawppy/lib/rover/wheel_ackermann.c
@@ -1,4 +1,15 @@
 #include "wheel_ackermann.h"
+/*
+ * @brief set all steering angle and speeds to zero
+ */
+void wheel_msg_reset(wheel_msg* pwheel_msg)
+{
+  for (int wheel = 0; wheel < WHEEL_MSG_DATA_COUNT; wheel++)
+  {
+    pwheel_msg->steer[wheel] = 0;
+    pwheel_msg->speed[wheel] = 0;
+  }
+}
 
 void wheel_ackermann_task(void* pvParameter)
 {
@@ -7,11 +18,7 @@ void wheel_ackermann_task(void* pvParameter)
 
   // Initialize wheel data output message
   wheelData.timeStamp = xTaskGetTickCount();
-  for (int wheel = 0; wheel < wheel_count; wheel++)
-  {
-    wheelData.steer[wheel] = 0;
-    wheelData.speed[wheel] = 0;
-  }
+  wheel_msg_reset(&wheelData);
 
   // Verify input parameters are not null.
   wheel_ackermann_task_parameters* pTaskParameters = (wheel_ackermann_task_parameters*) pvParameter;
@@ -30,6 +37,69 @@ void wheel_ackermann_task(void* pvParameter)
     // Wait for next velocity command
     if (pdTRUE == xQueueReceive(xCmdVelQueue, &cmdVelData, twist_msg_timeout_interval))
     {
+      if (0.0f != cmdVelData.angular.x ||
+          0.0f != cmdVelData.angular.y ||
+          0.0f != cmdVelData.linear.y ||
+          0.0f != cmdVelData.linear.z )
+      {
+        printf("WARNING: Velocity commanded along unsupported axes are ignored.\n");
+      }
+
+      float velocityLinear = cmdVelData.linear.x;
+      float velocityAngular = cmdVelData.angular.z;
+
+      wheel_msg_reset(&wheelData);
+
+      if (0 == cmdVelData.angular.z)
+      {
+        // Straight forward/back. All steering angles were set to zero by wheel_msg_reset
+        // so we only need to copy velocity numbers to all six wheels
+        for (int wheel = 0; wheel < wheel_count; wheel++)
+        {
+          wheelData.speed[wheel] = velocityLinear;
+        }
+      }
+      else
+      {
+        // Travel in an arc. Calculate center of the turn
+        float turnCenter = velocityLinear / velocityAngular;
+
+        // Compute speed and steering angle for each wheel
+        for (int wheel = 0; wheel < wheel_count; wheel++)
+        {
+          // Dimensions of a triangle representing a wheel relative to center of turn
+          float opposite = wheel_positions[wheel].x;
+          float adjacent = turnCenter - wheel_positions[wheel].y;
+          float hypotenuse = sqrt(pow(opposite, 2) + pow(adjacent, 2));
+
+          // Ackermann steering angle
+          if ( 0 == opposite )
+          {
+            // Wheels aligned with turning axis do not need to steer
+            wheelData.steer[wheel] = 0;
+          }
+          else
+          {
+            wheelData.steer[wheel] = atan(opposite / adjacent);
+          }
+
+          wheelData.speed[wheel] = velocityAngular * hypotenuse;
+          if ( 0 != turnCenter )
+          {
+            // When not turning in place, we need to fix up the sign because
+            // we lost the negative sign on wheel X/Y when squaring those
+            // values to calculate hypotenuse.
+            wheelData.speed[wheel] = copysign(wheelData.speed[wheel], velocityLinear);
+          }
+
+          // If the center of turn is inside the wheel, reverse motor speed.
+          if ((turnCenter > 0 && wheel_positions[wheel].y > 0 && wheel_positions[wheel].y > turnCenter) ||
+              (turnCenter < 0 && wheel_positions[wheel].y < 0 && wheel_positions[wheel].y < turnCenter))
+          {
+            wheelData.speed[wheel] *= -1;
+          }
+        }
+      }
 
       wheelData.timeStamp = xTaskGetTickCount();
       xQueueOverwrite(xWheelQueue, &wheelData);
--- esp32_sawppy/src/main.c
+++ esp32_sawppy/src/main.c
@@ -34,9 +34,9 @@ void app_main()
   else
   {
     xTaskCreate(joy_adc_read_task, "joy_adc_read_task", 2048, xJoystickQueue, 20, NULL);
-    xTaskCreate(joy_msg_print_task, "joy_msg_print_task", 2048, xJoystickQueue, 25, NULL);
+    //xTaskCreate(joy_msg_print_task, "joy_msg_print_task", 2048, xJoystickQueue, 25, NULL);
     xTaskCreate(joy_steer_task, "joy_steer_task", 2048, &joy_steer_params, 15, NULL);
-    xTaskCreate(twist_msg_print_task, "twist_msg_print_task", 2048, xCmdVelQueue, 25, NULL);
+    //xTaskCreate(twist_msg_print_task, "twist_msg_print_task", 2048, xCmdVelQueue, 25, NULL);
     xTaskCreate(wheel_ackermann_task, "wheel_ackermann_task", 2048, &wheel_ackermann_params, 14, NULL);
     xTaskCreate(wheel_msg_print_task, "wheel_msg_print_task", 2048, xWheelQueue, 25, NULL);
   }
