a3fb0c44809e0a39af95cf5d7468b9c073f8ee5e
Roger Cheng <roger.random@outlook.com>
2021-04-05 02:32:06 -0700

    Control pad

--- esp32_sawppy/browser_ui/static/wsplay.js
+++ esp32_sawppy/browser_ui/static/wsplay.js
@@ -1,7 +1,5 @@
-var steerControl;
-var speedControl;
+const updatePeriodMilliseconds = 100;
 var steerSpeedInterval;
-const updatePeriodMilliseconds = 500;
 
 var exampleSocket;
 
@@ -9,20 +7,29 @@ var statusElement;
 var statusString = "startup";
 
 const portraitRatio = 1.1;
-const knobRadius = 0.1;
 var drawCanvas;
 var ctx;
 var drawWidth;
 var drawHeight;
 
+const boxBorderFraction = 0.05;
+const boxLabelFraction = 0.04;
 var boxOffset;
 var boxSize;
+
+const knobRadiusFraction = 0.1;
+var knobRadius;
+var knobMin;
+var knobMax;
+var clampedX;
+var clampedY;
+var msgX = 0;
+var msgY = 0;
 
 var pointerCaptured = undefined;
 
 function eventSetup() {
-  //startSteerSpeed();
-  //startWebSocket();
+  startWebSocket();
   startControls();
 }
 
@@ -36,10 +43,15 @@ function startControls() {
 }
 
 function down_handler(pointerEvent) {
-  if (pointerCaptured === undefined && pointerEvent.isPrimary) {
-    updateStatus("Pointer Capture");
+  if (pointerCaptured === undefined &&
+      pointerEvent.isPrimary &&
+      pointerEvent.offsetX > clampedX-knobRadius &&
+      pointerEvent.offsetX < clampedX+knobRadius &&
+      pointerEvent.offsetY > clampedY-knobRadius &&
+      pointerEvent.offsetY < clampedY+knobRadius) {
     pointerCaptured = pointerEvent.pointerId;
     drawCanvas.setPointerCapture(pointerCaptured);
+    update_knob(pointerEvent);
   }
 }
 
@@ -47,14 +59,29 @@ function up_handler(pointerEvent) {
   if (pointerEvent.pointerId === pointerCaptured) {
     drawCanvas.releasePointerCapture(pointerCaptured);
     pointerCaptured = undefined;
-    updateStatus("Pointer Release");
+    update_knob(pointerEvent);
+
+    msgY = 0;
+    clampedY = knobMin + (knobMax-knobMin)/2;
   }
 }
 
 function move_handler(pointerEvent) {
   if (pointerCaptured == pointerEvent.pointerId) {
-    updateStatus("Pointer " + pointerEvent.clientX + ", " + pointerEvent.clientY);
+    update_knob(pointerEvent);
   }
+}
+
+function update_knob(pointerEvent) {
+  clampedX = Math.max(knobMin, Math.min(knobMax, pointerEvent.offsetX));
+  msgX = 2*(clampedX - knobMin)/(knobMax - knobMin) - 1;
+  msgX = Math.round(msgX*100)/100;
+
+  clampedY = Math.max(knobMin, Math.min(knobMax, pointerEvent.offsetY));
+  msgY = 2*(clampedY - knobMin)/(knobMax - knobMin) - 1;
+  msgY = Math.round(msgY*100)/100;
+
+  window.requestAnimationFrame(reDraw);
 }
 
 function reSize() {
@@ -74,8 +101,15 @@ function reSize() {
   drawCanvas.width = drawWidth;
   drawCanvas.height = drawHeight;
 
-  boxOffset = drawWidth*0.05;
-  boxSize = drawWidth*0.9;
+  boxOffset = drawWidth*boxBorderFraction;
+  boxSize = drawWidth*(1.0-boxBorderFraction*2);
+
+  knobRadius = boxSize*knobRadiusFraction;
+  knobMin = boxOffset + knobRadius;
+  knobMax = boxOffset + boxSize - knobRadius;
+
+  clampedX = knobMin + (msgX+1)*(knobMax-knobMin)/2;
+  clampedY = knobMin + (msgY+1)*(knobMax-knobMin)/2;
 
   window.requestAnimationFrame(reDraw);
 }
@@ -83,12 +117,40 @@ function reSize() {
 function reDraw() {
   ctx.clearRect(0,0,drawWidth, drawHeight);
   drawBox();
+  drawKnob();
   drawStatusText();
 }
 
 function drawBox() {
-  ctx.fillStyle = "#0000FF";
+  ctx.fillStyle = '#0000FF';
   ctx.fillRect(boxOffset, boxOffset, boxSize, boxSize);
+
+  var labelSize = drawWidth*boxLabelFraction;
+  var boxMid = boxOffset+(boxSize/2);
+  ctx.fillStyle = '#FFFFFF';
+  ctx.font =  labelSize + "px sans-serif";
+  ctx.textAlign = "center";
+  ctx.fillText('FORWARD', boxMid, boxOffset+labelSize);
+  ctx.fillText('REVERSE', boxMid, boxOffset+boxSize-labelSize);
+  ctx.textAlign = "left";
+  ctx.fillText('LEFT', boxOffset, boxMid);
+  ctx.textAlign = "right";
+  ctx.fillText('RIGHT', boxOffset+boxSize, boxMid);
+
+}
+
+function drawKnob() {
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(clampedX, clampedY, boxSize*knobRadiusFraction, 0, 2*Math.PI);
+  if (pointerCaptured === undefined) {
+    ctx.fillStyle = '#FF0000';
+  }
+  else {
+    ctx.fillStyle = '#00FF00';
+  }
+  ctx.fill();
+  ctx.restore();
 }
 
 function updateStatus(newStatusString) {
@@ -100,20 +162,15 @@ function drawStatusText() {
   var textSize = drawWidth*0.05;
   ctx.fillStyle = "#000000";
   ctx.font = textSize + "px sans-serif";
-  ctx.fillText("Status: " + statusString, textSize, drawWidth+textSize);
-}
-
-function startSteerSpeed() {
-  steerControl = document.getElementById('rangeSteer');
-  speedControl = document.getElementById('rangeSpeed');
-  statusElement = document.getElementById('status');
+  ctx.textAlign = "center";
+  ctx.fillText(statusString, drawWidth/2, drawWidth+textSize);
 }
 
 function updateSteerSpeed() {
   var joy_msg_axes = [0,0];
 
-  joy_msg_axes[0] = steerControl.value;
-  joy_msg_axes[1] = speedControl.value;
+  joy_msg_axes[0] =  msgX;
+  joy_msg_axes[1] = -msgY;
 
   message= { axes:  joy_msg_axes };
 
@@ -134,28 +191,28 @@ function startWebSocket() {
 }
 
 function onSocketOpen(event) {
-  updateStatus("WebSocket Open!");
+  updateStatus("Open");
   exampleSocket.send("joy_msg_send");
 }
 
 function onSocketError(event) {
-  updateStatus("Server Communication Error.");
+  updateStatus("Error");
   console.log(event);
 }
 
 function onSocketClose(event) {
-  updateStatus("Disconnected.");
+  updateStatus("Disconnected");
   stopUpdates();
 }
 
 function onSocketMessage(event) {
-  updateStatus("Received Server Command.");
+  updateStatus("Received");
   console.log(event.data);
   if (event.data === "joy_msg_receive")
   {
     stopUpdates(); // Stop earlier timers
     steerSpeedInterval = setInterval(updateSteerSpeed, updatePeriodMilliseconds);
-    updateStatus("Connected.");
+    updateStatus("Connected");
   }
 }
 
